d4e1a5ae6826107169225d73370f2fbaf5c33ad0
016723-RD1(李亦然) <luke.li@senao.com.tw>
2018-11-27 17:57:34 +0800

    add App2.vue

---
+++ src/App2.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <test></test>
+    <a href=""></a>
+  </div>
+</template>
