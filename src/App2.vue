22452835173ecf4d444ce41227ed280426303b86
016723-RD1(李亦然) <luke.li@senao.com.tw>
2018-12-28 11:48:39 +0800

    1.add vue.config.js 2.change code

--- src/App2.vue
+++ src/App2.vue
@@ -2,9 +2,12 @@
     <div id="hw1-2">
         <h1>HW1</h1>
         <component-hw1></component-hw1>
+        <b-btn variant="primary" href="https://bootstrap-vue.js.org/">More Info</b-btn>
     </div>
 </template>
 <script lang="ts">
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import componentHw1 from './components/HW1/componentHw1-2.vue';
 /*
 export default Vue.extend({
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import App2 from './App2.vue';
 import App3 from './App3.vue';
 
 // import HelloWorld from './components/HelloWorld.vue';
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
